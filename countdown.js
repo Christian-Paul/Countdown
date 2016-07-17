8ac280bf123ff977356c297cd8fbd3ab3e295433
Christian Paul <christiansamuelpaul@gmail.com>
2016-07-17 16:31:41 -0400

    Add user interface, and form validation. Users can now start a countdown to an arbitrary date

--- countdown.js
+++ countdown.js
@@ -5,10 +5,10 @@ function zeroPad(number) {
   } else {
     return '0'.concat(number);
   }
-};
+}
 
-function timeUntil(deadline) {
-  var ms = Date.parse(deadline) - Date.now();
+function timeUntil(userDate) {
+  var ms = userDate - Date.now();
   var seconds = Math.floor((ms/1000) % 60);
   var minutes = Math.floor((ms/(1000*60)) % 60);
   var hours = Math.floor((ms/(1000*60*60)) % 24);
@@ -23,15 +23,36 @@ function timeUntil(deadline) {
   };
 };
 
-$(document).ready(function() {  
+function startTimer(userDate) {
   var updateTimer = window.setInterval(updater, 1000);
   
   function updater() {
     var stonyBrookMoveIn = 'August 26, 2016 16:00:00';
-    var timeLeft = timeUntil(stonyBrookMoveIn);
-    $('.days-data').text(timeLeft.days);
-    $('.hours-data').text(timeLeft.hours);
-    $('.minutes-data').text(timeLeft.minutes);
-    $('.seconds-data').text(timeLeft.seconds);
+    var timeLeft = timeUntil(userDate);
+    $('.days-data').text(zeroPad(timeLeft.days));
+    $('.hours-data').text(zeroPad(timeLeft.hours));
+    $('.minutes-data').text(zeroPad(timeLeft.minutes));
+    $('.seconds-data').text(zeroPad(timeLeft.seconds));
   };
+}
+
+$(document).ready(function() {
+  $('.post-click').hide();
+  $('.error-message').hide();
+  
+  $('.submit').click(function() {
+    var userMonth = $('.month').val();
+    var userDay = $('.day').val();
+    var userYear = $('.year').val();
+    var userDate = userMonth + '/' + userDay + '/' + userYear;
+    var userDate = Date.parse(userDate)
+    
+    if (userDate && userDate > Date.now()) {
+      startTimer(userDate)
+      $('.pre-click').hide();
+      $('.post-click').show();
+    } else {
+      $('.error-message').show();
+    }
+  })
 });
